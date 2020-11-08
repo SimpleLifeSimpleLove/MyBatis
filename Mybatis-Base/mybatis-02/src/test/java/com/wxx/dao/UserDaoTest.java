a2899907547dab5c2c124722a2aaf0058833feae
SimpleLifeSimpleLove <1137247975@qq.com>
2020-11-08 22:00:54 +0800

    mybatis-02

---
+++ Mybatis-Base/mybatis-02/src/test/java/com/wxx/dao/UserDaoTest.java
@@ -0,0 +1,33 @@
+package com.wxx.dao;
+
+import com.wxx.pojo.User;
+import com.wxx.utils.MyBatisUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * Date: 2020/11/8 19:51
+ * Content:
+ * Test不能运行的问题：idea的原因!!! 不要用 idea2020.1!!!
+ */
+public class UserDaoTest {
+
+    @Test
+    public void testGetList() {
+        // 第一步：获取 SqlSession 对象
+        SqlSession sqlSession = MyBatisUtils.getSqlSession();
+
+        // 第二步：执行SQL. 方式一 : getMapper
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);  // mapper
+        List<User> userList = mapper.getList();
+//        userList.forEach(System.out::println);
+        for (User user : userList) {
+            System.out.println(user);
+        }
+
+        // 第三步：关闭 SqlSession
+        sqlSession.close();
+    }
+}
