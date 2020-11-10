9c60c5885d7188226f30719b230bce3514abafed
SimpleLifeSimpleLove <1137247975@qq.com>
2020-11-10 18:53:14 +0800

    SqlSessionFactoryBuilder SqlSessionFactory SqlSession

--- Mybatis-Base/mybatis-03/src/main/java/com/wxx/utils/MyBatisUtils.java
+++ Mybatis-Base/mybatis-03/src/main/java/com/wxx/utils/MyBatisUtils.java
@@ -13,6 +13,20 @@ import java.io.InputStream;
  * Content: sqlSessionFactory --> sqlSession
  */
 public class MyBatisUtils {
+    /*
+        SqlSessionFactoryBuilder :
+            一旦创建了 SqlSessionFactory，就不再需要它了
+            局部变量
+        SqlSessionFactory :
+            说白了就是可以想象为 ：数据库连接池
+            SqlSessionFactory 一旦被创建就应该在应用的运行期间一直存在，没有任何理由丢弃它或重新创建另一个实例。
+            因此 SqlSessionFactory 的最佳作用域是应用作用域。
+            最简单的就是使用单例模式或者静态单例模式。
+        SqlSession :
+            连接到连接池的一个请求！
+            SqlSession 的实例不是线程安全的，因此是不能被共享的，所以它的最佳的作用域是请求或方法作用域。
+            用完之后需要赶紧关闭，否则资源被占用！
+     */
 
     private static SqlSessionFactory sqlSessionFactory;
 
