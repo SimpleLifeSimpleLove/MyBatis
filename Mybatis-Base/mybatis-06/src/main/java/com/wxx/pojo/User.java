b8ffaa93d63d9013ce47c23ca61545864e1a50c8
SimpleLifeSimpleLove <1137247975@qq.com>
2020-11-11 20:36:30 +0800

    lombok

--- Mybatis-Base/mybatis-06/src/main/java/com/wxx/pojo/User.java
+++ Mybatis-Base/mybatis-06/src/main/java/com/wxx/pojo/User.java
@@ -1,5 +1,8 @@
 package com.wxx.pojo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.apache.ibatis.type.Alias;
 
 /**
@@ -7,51 +10,12 @@ import org.apache.ibatis.type.Alias;
  * Content:
  */
 @Alias("User")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
     private int id;
     private String name;
     private String password;  // 这个字段和数据库中的字段不一致，数据库中为pwd，根据id查询用户，会导致该字段password=null
-
-    public User() {
-    }
-
-    public User(int id, String name, String password) {
-        this.id = id;
-        this.name = name;
-        this.password = password;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
 }
